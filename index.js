e4999df6af71f79bb566c23049ee47d02c8f9712
ccliff <charlie.cliff@gmail.com>
2017-01-22 08:54:46 -0600

    test

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/main');
-});
+// app.get('/', function(request, response) {
+  // response.render('pages/main');
+// });
+
+app.get('/', function (req, res) { res.send('Hello'); });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
