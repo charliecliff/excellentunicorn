5b1766c6306ced453c7721d3e2e8957f86abd68a
ccliff <charlie.cliff@gmail.com>
2017-01-23 18:15:54 -0600

    Got the Slideshow of Products working

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.get('/', function(request, response) {
   response.render('pages/main');
 });
 
-// app.get('/', function (req, res) { res.send('Hello'); });
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
